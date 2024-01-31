46128e9169d3c4be75e52fed05d020295ca890c8
Abdul Azis <abdul.azizz1108@gmail.com>
2024-01-31 17:16:42 +0700

    add:not found page created

---
+++ client/src/presentation/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+    return (
+        <div className='flex justify-center items-center min-h-screen gap-3'>
+            <h1 className='text-xl font-medium'>404</h1>
+            <h3 className='text-lg font-medium'>Page Not Found</h3>
+        </div>
+    );
+};
+
+export default NotFoundPage;
