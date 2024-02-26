042a1402b24d874bed8c88b43889cbd4692e0378
Abdul Azis <abdul.azizz1108@gmail.com>
2024-02-26 10:09:01 +0700

    feat(auth): fix useLoggedUser hooks logic

--- client/src/common/hooks/useLoggedUser.tsx
+++ client/src/common/hooks/useLoggedUser.tsx
@@ -2,8 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '.';
-import UserRemoteDataSource from '../../data/datasources/UserRemoteDataSource';
-import { setUser } from '../../presentation/redux/slices/userSlice';
 
 export const useLoggedUser = () => {
     const navigate = useNavigate();
@@ -12,18 +10,8 @@ export const useLoggedUser = () => {
     const user = useAppSelector((state) => state.user.user);
 
     useEffect(() => {
-        const fetchLoggedUser = async () => {
-            try {
-                const token = localStorage.getItem('token') ?? '';
-                const user = await UserRemoteDataSource.getLoggedUser(token);
-                dispatch(setUser(user));
-            } catch (error) {
-                navigate('/login');
-            }
-        };
-
         if (!user) {
-            fetchLoggedUser();
+            navigate('/login');
         }
     }, [user, navigate, dispatch]);
 
--- client/src/presentation/components/Layouts/IndexLayout.tsx
+++ client/src/presentation/components/Layouts/IndexLayout.tsx
@@ -1,10 +1,10 @@
 import { useMemo } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Role } from '../../../common/enum';
-import { useAppSelector } from '../../../common/hooks';
+import { useLoggedUser } from '../../../common/hooks/useLoggedUser';
 
 const IndexLayout = () => {
-    const user = useAppSelector((state) => state.user.user);
+    const user = useLoggedUser();
 
     const indexElement = useMemo(() => {
         return user?.role?.toLowerCase() === Role.ADMIN_LSP ? (
